d28c67a792787046daa290b3ba4ec70005ab5e0f
Cezkarma <joshcoetzer1997@gmail.com>
2026-06-12 23:29:12 +0200

    UI done and enemy animations

--- SA1 Top Down Shooter/Assets/Scripts/SceneLoader.cs
+++ SA1 Top Down Shooter/Assets/Scripts/SceneLoader.cs
@@ -5,11 +5,13 @@ public class SceneLoader : MonoBehaviour
 {
     public void LoadSceneByName(string sceneName)
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene(sceneName);
     }
 
     public void LoadSceneByIndex(int sceneIndex)
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene(sceneIndex);
     }
 
